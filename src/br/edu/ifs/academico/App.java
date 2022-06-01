f4e5e8302d28dac1f45d65cb069f523e64334703
IuryCc <iurycauan0352@hotmail.com>
2022-06-01 20:05:34 -0300

    ok

---
+++ src/br/edu/ifs/academico/App.java
@@ -0,0 +1,9 @@
+package br.edu.ifs.academico;
+
+public class App {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        System.out.println(menu.getMenu());
+    }
+}
